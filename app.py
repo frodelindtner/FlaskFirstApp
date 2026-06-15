b8b913187ed6fe6b1d7725422ad4f1a4bc9d8fb5
frodelindtner <frodelindtner@hotmail.com>
2026-06-15 09:51:17 +0200

    init

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return '<h1>Greetings, traveler! Stay a while and listen...</h1>'
+
+
+
+if __name__=='__main__':
+    app.run(debug = True)
