cd94d0807fdcb7e45c1a6e01440a4d47e6e79262
frodelindtner <frodelindtner@hotmail.com>
2026-06-15 10:37:37 +0200

    More pages and more templating

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Velkommen besøgende</h1>'
+    return render_template('index.html', title = 'Velkommen!', name = 'Jens Jakob Jensen')
+    # return '<h1>Velkommen besøgende</h1>'
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'Om site')
 
 
 if __name__=='__main__':
