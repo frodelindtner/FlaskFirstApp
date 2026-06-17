83c0861d9965188de9466f00e5a41eb890a7504b
frodelindtner <frodelindtner@hotmail.com>
2026-06-17 12:03:30 +0200

    test files added

---
+++ services/customer_service_test.py
@@ -0,0 +1,12 @@
+import unittest
+from customer_service import CustomerService
+
+class Testing(unittest.TestCase):
+    def test_get_all_customers(self):
+        # Arrange
+        # Act
+        # Assert
+        self.assertFalse(False)
+
+
+unittest.main()
---
+++ storage/db_customers_test.py
@@ -0,0 +1,18 @@
+import unittest
+from db_customers import Db_Customers
+
+
+class Testing(unittest.TestCase):
+    def test_get_all_customers(self):
+        # Arrange
+        # Act
+        # Assert
+        self.assertFalse(False)
+
+    def test_create_customer(self):
+        # Arrange
+        # Act
+        # Assert
+        self.assertFalse(False)        
+
+unittest.main()
