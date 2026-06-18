b9f8b8f6f66237156dffd1eb5d251911131baa4f
frodelindtner <frodelindtner@hotmail.com>
2026-06-18 11:22:36 +0200

    mainly bootstrap html markup

--- app.py
+++ app.py
@@ -7,31 +7,31 @@ from services.product_service import ProductService
 
 app = Flask(__name__)
 album_service = Service()
-album_service.create_some_objects()
-
 stat_service = Stat_Service()
-stat_service.create_some_objects()
-
 customer_service = CustomerService()
-# has customer data in data
 order_service = OrderService()
 product_service = ProductService()
+
+stat_service.create_some_objects()
+album_service.create_some_objects()
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html', title = 'Velkommen!', name = 'Jens Jakob Jensen')
+    return render_template('index.html', title = 'Velkommen!', description = 'Velkommen til denne side', name = 'Jens Jakob Jensen')
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title = 'Om site')
+    return render_template('about.html', title = 'Om site', description = 'Her kan du læse om denne side' )
 
 # --------CUSTOMER-----------------------------------------------------------------
 
 @app.route('/customers')
 def customers():
     customers = customer_service.get_all_customers()
-    return render_template('/customers/customers.html', cus_list = customers)
+    return render_template('/customers/customers.html', title = 'liste over kunder', 
+                           description = 'Overblik over kunder klik evt. detaljer', cus_list = customers)
 
 @app.route('/customers/create', methods=['GET', 'POST'])
 def create_customer():
@@ -107,15 +107,20 @@ def create_order():
         return render_template('orders/create-order.html', customers = customers, products = products)
 
 # ------------------------------------------------------------------------------------------------
-@app.route('/albums')
+# @app.route('/albums')
+# def all_albums():
+#     return render_template('albums/albums.html', title = 'Alle albums', description = 'liste visning', albums = album_service.get_all_albums())
+
+@app.route('/alle-albums')
 def all_albums():
-    return render_template('albums.html', title = 'Alle albums', albums = album_service.get_all_albums())
+    return render_template('albums/albums.html', title = 'Alle albums', description = 'liste visning', albums = album_service.get_all_albums())
+
 
 @app.route('/albums/<id>')
 def album_by_entry(id):
     album = album_service.find_album_by_id(int(id))
     if album is not None:
-        return render_template('album-details.html', album = album)
+        return render_template('albums/album-details.html', album = album)
     return '<h1>Intet album kunne findes</h1>'
     
 @app.route('/albums/<id>/edit', methods=['GET', 'POST'])
@@ -129,7 +134,7 @@ def edit_album(id):
             updated_album = album_service.update_album(album)
             return redirect(url_for("album_by_entry", id = updated_album.id))
         else:
-            return render_template('edit-album.html', album = album)
+            return render_template('albums/edit-album.html', album = album)
     else:
         return '<h1>Findes ikke!</h1>'
     
@@ -141,7 +146,7 @@ def delete_album(id):
             album_service.delete_album(album.id)
             return redirect(url_for("all_albums"))
         else:
-            return render_template('delete-album.html', album = album, title='sletning')
+            return render_template('albums/delete-album.html', album = album, title='sletning')
     else:
         return '<h1>Album er allerede slettet</h1>'
 
